81c67340ebb62f6b8e6268bd5f1b56dac5663e1a
Alfredo Vasquez <vasquez.alf@gmail.com>
2019-10-01 22:28:33 -0500

    ultimas ligas

---
+++ page-pkm-league-inter-2019.php
@@ -0,0 +1,290 @@
+<?php //Template Name: PKM Liga Intertienda 2019
+get_header();  ?>
+<div id="main" class="group">
+	<section id="intertiendas">
+		<h1 class="page-title"><?php the_title(); ?></h1>
+		<div class="liga-content" style="text-align:justify;">
+			<?php the_content(); ?>
+		</div>
+		<div class="liga-images">
+			<div class="liga-content">
+				<h2>Fechas</h2>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Liga Intertiendas Mayo - Calendario.png' ?>" alt="Calendario Mayo" />
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Liga Intertiendas Junio - Calendario.png' ?>" alt="Calendario Junio" />
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Liga Intertiendas Julio - Calendario.png' ?>" alt="Calendario Julio" />
+			</div>
+			<div class="liga-content">
+				<h2>Resultados</h2>
+				<p class="text">Luego de las 8 semanas de competencia clasificaron los siguientes entrenadores:</p>
+				<table class="tabla_top">
+				  <tr class="tr_fila">
+				    <th class="th_cabeza">Ranking</th>
+				    <th class="th_cabeza">Entrenador</th>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">1er Puesto Acumulado</td>
+				    <td class="td_celda">Ángel Aranibar</td>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">2do Puesto Acumulado</td>
+				    <td class="td_celda">Jorge Castañeda</td>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">3er Puesto Acumulado</td>
+				    <td class="td_celda">Luis Delgado</td>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">4to Puesto Acumulado</td>
+				    <td class="td_celda">Renzo Zambrano</td>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">1er Puesto Magic Place</td>
+				    <td class="td_celda">Jorge Coronel</td>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">2do Puesto El Tablero de Otto</td>
+				    <td class="td_celda">Álvaro Castañeda</td>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">5to Puesto NLG</td>
+				    <td class="td_celda">Sebastián Casas</td>
+				  </tr>
+				  <tr class="tr_fila">
+				    <td class="td_celda">3er Puesto GD Games</td>
+				    <td class="td_celda">Miguel Chozo</td>
+				  </tr>
+				</table>
+				<p class="text">Así quedó el Top 8 de cada tienda:</p>
+				<div class="central-block-sb">
+					<table class="tabla_tienda">
+					  <tr class="tr_fila">
+					    <th class="th_cabeza"></th>
+					    <th class="th_cabeza">El Tablero de Otto</th>
+					    <th class="th_cabeza"></th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <th class="th_cabeza">Ranking</th>
+					    <th class="th_cabeza">Entrenador</th>
+					  	<th class="th_cabeza">Puntos</th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">1</td>
+					    <td class="td_celda">Jorge Castañeda</td>
+					    <td class="td_celda">84</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">2</td>
+					    <td class="td_celda">Álvaro Castañeda</td>
+					    <td class="td_celda">70</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">3</td>
+					    <td class="td_celda">Luis Delgado</td>
+					    <td class="td_celda">60</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">4</td>
+					    <td class="td_celda">Renzo Zambrano</td>
+					    <td class="td_celda">60</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">5</td>
+					    <td class="td_celda">Mario De la Cruz</td>
+					    <td class="td_celda">54</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">6</td>
+					    <td class="td_celda">Guillermo Reategui</td>
+					    <td class="td_celda">54</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">7</td>
+					    <td class="td_celda">Axel Venturo</td>
+					    <td class="td_celda">52</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">8</td>
+					    <td class="td_celda">>Sebastián Casas</td>
+					    <td class="td_celda">48</td>
+					  </tr>
+					</table>
+					<table class="tabla_tienda">
+					  <tr class="tr_fila">
+					    <th class="th_cabeza"></th>
+					    <th class="th_cabeza">Magic Place</th>
+					    <th class="th_cabeza"></th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <th class="th_cabeza">Ranking</th>
+					    <th class="th_cabeza">Entrenador</th>
+					  	<th class="th_cabeza">Puntos</th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">1</td>
+					    <td class="td_celda">Jorge Coronel</td>
+					    <td class="td_celda">82</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">2</td>
+					    <td class="td_celda">Ángel Aranibar</td>
+					    <td class="td_celda">80</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">3</td>
+					    <td class="td_celda">Joaquín Orrego</td>
+					    <td class="td_celda">70</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">4</td>
+					    <td class="td_celda">Renzo Zambrano</td>
+					    <td class="td_celda">66</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">5</td>
+					    <td class="td_celda">Santiago García</td>
+					    <td class="td_celda">58</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">6</td>
+					    <td class="td_celda">Fabrizio Inga</td>
+					    <td class="td_celda">50</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">7</td>
+					    <td class="td_celda">Yota Mori</td>
+					    <td class="td_celda">48</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">8</td>
+					    <td class="td_celda">Luis Delgado</td>
+					    <td class="td_celda">46</td>
+					  </tr>
+					</table>
+					<table class="tabla_tienda">
+					  <tr class="tr_fila">
+					    <th class="th_cabeza"></th>
+					    <th class="th_cabeza">NLG</th>
+					    <th class="th_cabeza"></th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <th class="th_cabeza">Ranking</th>
+					    <th class="th_cabeza">Entrenador</th>
+					  	<th class="th_cabeza">Puntos</th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">1</td>
+					    <td class="td_celda">Ángel Aranibar</td>
+					    <td class="td_celda">92</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">2</td>
+					    <td class="td_celda">Jorge Castañeda</td>
+					    <td class="td_celda">80</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">3</td>
+					    <td class="td_celda">Renzo Zambrano</td>
+					    <td class="td_celda">74</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">4</td>
+					    <td class="td_celda">Luis Delgado</td>
+					    <td class="td_celda">62</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">5</td>
+					    <td class="td_celda">Sebastián Casas</td>
+					    <td class="td_celda">62</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">6</td>
+					    <td class="td_celda">John Cartagena</td>
+					    <td class="td_celda">42</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">7</td>
+					    <td class="td_celda">Niolas Yaji</td>
+					    <td class="td_celda">40</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">8</td>
+					    <td class="td_celda">Axel Rivera</td>
+					    <td class="td_celda">30</td>
+					  </tr>
+					</table>
+					<table class="tabla_tienda">
+						<tr class="tr_fila">
+					    <th class="th_cabeza"></th>
+					    <th class="th_cabeza">GD Games</th>
+					    <th class="th_cabeza"></th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <th class="th_cabeza">Ranking</th>
+					    <th class="th_cabeza">Entrenador</th>
+					  	<th class="th_cabeza">Puntos</th>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">1</td>
+					    <td class="td_celda">Ángel Aranibar</td>
+					    <td class="td_celda">54</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">2</td>
+					    <td class="td_celda">Jorge Coronel</td>
+					    <td class="td_celda">54</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">3</td>
+					    <td class="td_celda">Miguel Chozo</td>
+					    <td class="td_celda">45</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">4</td>
+					    <td class="td_celda">Jorge Isuhuaylas</td>
+					    <td class="td_celda">31</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">5</td>
+					    <td class="td_celda">Nicolás Canevaro</td>
+					    <td class="td_celda">28</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">6</td>
+					    <td class="td_celda">Franco Arevalo</td>
+					    <td class="td_celda">24</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">7</td>
+					    <td class="td_celda">Carlo Sergio Morey</td>
+					    <td class="td_celda">20</td>
+					  </tr>
+					  <tr class="tr_fila">
+					    <td class="td_celda">8</td>
+					    <td class="td_celda">Rodrigo Pereda</td>
+					    <td class="td_celda">18</td>
+					  </tr>
+					</table>
+				</div>
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Liga Intertiendas - Resultados 1.jpg' ?>" alt="Resultados Acumulados" />
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Liga Intertiendas - Resultados 2.jpg' ?>" alt="Resultados Acumulados" />
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Liga Intertiendas - Resultados 3.jpg' ?>" alt="Resultados Acumulados" />
+			</div>
+			<div class="liga-content">
+				<h2>Llaves del Top 8</h2>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Intertiendas 2019 - Top 8.jpg' ?>" alt="Mesas Top8" />
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Intertiendas 2019 - Top 4.jpg' ?>" alt="Mesas Top4" />
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Intertiendas 2019 - Final.jpg' ?>" alt="Gran Final" />
+				<br>
+				<img src="<?php print IMAGES.'/Otto Pokémon League - Liga Intertiendas - Top8 Brackets.png' ?>" alt="Llaves del Top8" />
+			</div>
+		</div>
+	</section>
+</div>
+<?php get_footer(); ?>
--- page-pkm-league-may-19.php
+++ page-pkm-league-may-19.php
@@ -1,4 +1,4 @@
-<?php //Template Name: PKM League Abr19
+<?php //Template Name: PKM League May19
 get_header();  ?>
 <div id="main" class="group">
 	<section id="intertiendas">
@@ -12,15 +12,15 @@ get_header();  ?>
 		<div class="liga-images">
 			<div class="liga-content">
 				<h2>Fechas</h2>
-				<img src="<?php print IMAGES.'/Otto Pokémon League Abril 19 - Calendario.png' ?>" alt="Calendario" />
+				<img src="<?php print IMAGES.'/Otto Pokémon League Mayo 19 - Calendario.png' ?>" alt="Calendario" />
 			</div>
 			<div class="liga-content">
 				<h2>Resultados</h2>
-				<img src="<?php print IMAGES.'/Otto Pokémon League Abril 19 - Resultados.png' ?>" alt="Resultados de la Liga" />
+				<img src="<?php print IMAGES.'/Otto Pokémon League Mayo 19 - Resultados.png' ?>" alt="Resultados de la Liga" />
 			</div>
 			<div class="liga-content">
 				<h2>Brackets Top 8</h2>
-				<img src="<?php print IMAGES.'/Otto Pokémon League Abril 19 - Brackets Top 8.png' ?>" alt="Brackets Top8" />
+				<img src="<?php print IMAGES.'/Otto Pokémon League Mayo 19 - Brackets Top 8.png' ?>" alt="Brackets Top8" />
 			</div>
 		</div>
 	</section>
